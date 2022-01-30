64d94e9892839a81f826c6f1e9d67f0c93b01cfa
Rohit Vishwakarma <53476944+Rohit77707@users.noreply.github.com>
2022-01-30 13:29:37 +0530

    Add files via upload

---
+++ Jan-24/covid-data.js
@@ -0,0 +1,27 @@
+var url = "https://api2.binance.com/api/v3/ticker/24hr";
+var div = document.getElementById("tab");
+var table = document.getElementById("table");
+
+fetch(url)
+  .then((Response) => Response.json())
+  .then((data) => {
+    console.log(data);
+    for (let i = 0; i <= 10; i++) {
+      var th1 = document.createElement("th");
+      var th2 = document.createElement("th");
+      var th3 = document.createElement("th");
+      var th4 = document.createElement("th");
+      var br=document.createElement("br")
+      th1.innerText = "Symbol";
+      th2.innerText = "Bid Qty";
+      table.append(th1, th2, th3, th4);
+      var tr = document.createElement("tr");
+      var td1 = document.createElement("td");
+      var td2 = document.createElement("td");
+      td1.innerText = data[i].symbol;
+      td2.innerText = data[i].bidQty;
+      tr.append(td1, td2);
+      tr.appendChild(br);
+      table.appendChild(tr);
+    }
+  });
